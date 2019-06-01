e3902a9f611bb747fc8d7aca00ea968c23717a1d
henrytien <tianhangyu8886@gmail.com>
2019-06-01 17:23:50 +0800

    update

--- LeetCode/141. Linked List Cycle.cpp
+++ LeetCode/141. Linked List Cycle.cpp
@@ -1,59 +1,55 @@
-#include"AllInclude.h"
-//https://leetcode.com/problems/linked-list-cycle/
+#include "AllInclude.h"
+// https://leetcode.com/problems/linked-list-cycle/
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	bool hasCycle(ListNode *head) {
-		if (head == NULL) return false;
-
-		while (head->next)
-		{
-			if(!head->next->next)
-				break;
-			if (head->next == head->next->next)
-				return true;
-			head->next = head->next->next;
-			head = head->next;
-		}
-		return false;
-	}
-
-	
-
+  static bool has_cycle(ListNode *head);
 };
 
+bool Solution::has_cycle(ListNode *head) {
+  if (head == nullptr)
+    return false;
+
+  while (head->next) {
+    if (!head->next->next)
+      break;
+    if (head->next == head->next->next)
+      return true;
+    head->next = head->next->next;
+    head = head->next;
+  }
+  return false;
+}
+
 void push(ListNode **head, int val) {
-	ListNode *new_node = (ListNode*)malloc(sizeof(ListNode));
-	new_node->val = val;
-	new_node->next = *head;
-	*head = new_node;
+  auto *new_node = (ListNode *)malloc(sizeof(ListNode));
+  new_node->val = val;
+  new_node->next = *head;
+  *head = new_node;
 }
 
-void print(ListNode *list)
-{
-	while (list != NULL)
-	{
-		printf("%d ", list->val);
-		list = list->next;
-	}
+void print(ListNode *list) {
+  while (list != nullptr) {
+    printf("%d ", list->val);
+    list = list->next;
+  }
 }
 
-int main()
-{
-	vector<int> src = { 3,2,0,-4 };
-	ListNode *list = NULL;
-	for (auto &iter : src)
-		push(&list, iter);
-	print(list);
+int main() {
+  vector<int> src = {3, 2, 0, -4};
+  ListNode *list = NULL;
+  for (auto &iter : src)
+    push(&list, iter);
+  print(list);
 
-	cout << Solution().hasCycle(list);
-	getchar();
-	return 0;
+  cout << Solution().has_cycle(list);
+  getchar();
+  return 0;
 }
