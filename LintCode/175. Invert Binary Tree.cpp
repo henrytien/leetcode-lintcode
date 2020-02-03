bcf1bdfb9294ae65329a9f6bd8c2a806287b21fa
henrytine <tianhangyu8886@gmail.com>
2020-02-03 09:53:55 +0800

    Update 175. Invert Binary Tree.cpp

--- LintCode/175. Invert Binary Tree.cpp
+++ LintCode/175. Invert Binary Tree.cpp
@@ -35,3 +35,48 @@ public:
             dfs(right);
     }
 };
+
+
+// no-recursion
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void invertBinaryTree(TreeNode * root) {
+        if(NULL == root){
+            return;
+        }
+        
+        std::stack<TreeNode*> stack;
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode *node = stack.top();
+            stack.pop();
+            
+            swap(node->left,node->right);
+            if(node->left){
+                stack.push(node->left);
+            }
+             if(node->right){
+                stack.push(node->right);
+            }
+        }
+    }
+    
+    
+};
