2b67f5b14882ee3f16ae174670623bfdf0f4976e
henrytine <tianhangyu8886@gmail.com>
2020-02-03 09:34:31 +0800

    Create 175. Invert Binary Tree.cpp

---
+++ LintCode/175. Invert Binary Tree.cpp
@@ -0,0 +1,37 @@
+// https://www.lintcode.com/problem/invert-binary-tree/leaderboard
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void invertBinaryTree(TreeNode * root) {
+        if(NULL == root){
+            return;
+        }
+        dfs(root);
+    }
+    
+    void dfs(TreeNode *node){
+        TreeNode *left = node->left,*right = node->right;
+        node->left = right;
+        node->right = left;
+        if(left)
+            dfs(left);
+        if(right)
+            dfs(right);
+    }
+};
