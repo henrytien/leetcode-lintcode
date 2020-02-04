f7f4503b130b6322d4def5c56a9a06eaaa4d47e7
henrytine <tianhangyu8886@gmail.com>
2020-02-04 10:48:15 +0800

    Update 95. Validate Binary Search Tree.cpp

--- LintCode/95. Validate Binary Search Tree.cpp
+++ LintCode/95. Validate Binary Search Tree.cpp
@@ -12,33 +12,120 @@
  * }
  */
 
-class Solution {
+class Solution
+{
 public:
     /**
      * @param root: The root of binary tree.
      * @return: True if the binary tree is BST, or false
      */
-    bool isValidBST(TreeNode * root) {
-       inorder(root);
-       int size = res.size();
-       for (int i = 0; i < size - 1; i++) {
-           if(res[i] >= res[i+1]){
-               return false;
-           }
-       }
-       return true;
+    bool isValidBST(TreeNode *root)
+    {
+        inorder(root);
+        int size = res.size();
+        for (int i = 0; i < size - 1; i++)
+        {
+            if (res[i] >= res[i + 1])
+            {
+                return false;
+            }
+        }
+        return true;
     }
-    
-    void inorder(TreeNode *root){
-        if(NULL == root){
+
+    void inorder(TreeNode *root)
+    {
+        if (NULL == root)
+        {
             return;
         }
-        
+
         inorder(root->left);
         res.push_back(root->val);
         inorder(root->right);
     }
-    
-    private:
+
+private:
     std::vector<int> res;
+};
+
+/* Divide && Conquer*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isValidBST(TreeNode *root)
+    {
+
+        return helper(root, (long)INT_MIN - 1, (long)INT_MAX + 1);
+    }
+
+    bool helper(TreeNode *root, long min, long max)
+    {
+        if (NULL == root)
+        {
+            return true;
+        }
+
+        if (root->val <= min || root->val >= max)
+        {
+            return false;
+        }
+
+        bool left = helper(root->left, min, (long)root->val);
+        bool right = helper(root->right, (long)root->val, max);
+
+        return left && right;
+    }
+};
+
+/* stack no-recursion */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool isValidBST(TreeNode *root)
+    {
+        stack<TreeNode *> stack;
+        TreeNode *cur = root;
+
+        while (!stack.empty() || cur)
+        {
+            while (cur)
+            {
+                stack.push(cur);
+                cur = cur->left;
+            }
+            cur = stack.top();
+            stack.pop();
+            if (pre && pre->val >= cur->val)
+            {
+                return false;
+            }
+            pre = cur;
+            cur = cur->right;
+        }
+        return true;
+    }
+
+private:
+    TreeNode *pre;
 };
