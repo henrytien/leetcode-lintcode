8257aad1e6ec9c3ab47ea8b6b63d884f46b9002f
henrytine <tianhangyu8886@gmail.com>
2020-02-16 21:05:40 +0800

    Create 125. Valid Palindrome.cpp

---
+++ LeetCode/125. Valid Palindrome.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/valid-palindrome/
+// two pointer
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       int start = 0, end = s.size() -1;
+       while(start < end){
+           while(start < end && !isalnum(s[start]))
+               ++start;
+        
+           while(start < end && !isalnum(s[end]))
+                --end;   
+           if(start < end && tolower(s[start++]) != tolower(s[end--])){
+               return false;
+           }
+       }
+       return true;
+    }
+};
