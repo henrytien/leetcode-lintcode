e93a8b36aef6aa89fc429ec5e88f3ed056f4e46e
henrytine <tianhangyu8886@gmail.com>
2020-03-20 13:48:00 +0800

    Update 2.AddTwoNumbers.cpp

--- LeetCode/2.AddTwoNumbers.cpp
+++ LeetCode/2.AddTwoNumbers.cpp
@@ -8,68 +8,72 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-void print(ListNode *node){
-    while(node){
-        cout << node->val << endl;
-        node = node->next;
-    }
-}
-class Solution {
+class Solution{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL)
-            return l2;
-        if(l2 == NULL)
-            return l1;
+    ListNode* addList(ListNode* l1, ListNode* l2){
         ListNode* head = new ListNode(0);
-        ListNode* tail = head;
+        ListNode* curr = head;
         int dig = 0;
         while(l1 || l2){
-            
-            int total = 0;
-            total += l1 == NULL ? 0 : l1->val ;
-            total += l2 == NULL ? 0 : l2->val ;
-            total += dig;
-            dig = total / 10;
-            
-            ListNode* s = new ListNode(total%10);
-            tail->next = s;
-            tail = tail->next;
-            
+            int sum = 0;
+            sum += l1 == NULL ? 0 : l1->val;
+            sum += l2 == NULL ? 0 : l2->val;
+            sum += dig;
+
+            dig = sum / 10;
+            ListNode* tmp = new ListNode(sum % 10);
+            curr->next = tmp;
+            curr = tmp;
+
             if(l1) l1 = l1->next;
             if(l2) l2 = l2->next;
         }
-        // 5 5 = 0,1
+
         if(dig){
-            ListNode* s = new ListNode(dig);
-            tail->next = s;
-            tail = s;
+            ListNode* tmp = new ListNode(dig);
+            curr->next = tmp;
+            curr = tmp;
         }
         return head->next;
     }
 };
 
-ListNode* create_list(vector<int> &nums){
-    ListNode* tail, *head = new ListNode(0);
-    tail = head;
+ListNode* create_list_by_tail(vector<int>& nums){
+    if(nums.size() == 0){
+        return NULL;
+    }
+
+    ListNode* head = new ListNode(0);
+    ListNode* tail= head;
+  
     for (int i = 0; i < nums.size(); i++) {
-        ListNode* s = new ListNode(0);
-        s->val = nums[i];
+        ListNode* s = new ListNode(nums[i]);
         tail->next = s;
         tail = s;
     }
     return head->next;
 }
 
-int main(){
-    vector<int> nums = {5};
-    vector<int> nums1 = {5};
+void print_list(ListNode* node){
+    if(node == NULL){
+        return;
+    }
+    while(node){
+        cout << node->val << endl;
+        node = node->next;
+    }
+}
 
-//    vector<int> nums = {2,4,3};
-//    vector<int> nums1 = {5,6,4};
-    ListNode* l1 = create_list(nums);
-    ListNode* l2 = create_list(nums1);
+int main(){
     
+    vector<int> n = {5};
+    vector<int> n1 = {5};
+//  print_list(create_list_by_tail(n));
+    ListNode* l1 = create_list_by_tail(n);
+    ListNode* l2 = create_list_by_tail(n1);
     Solution s;
-    print(s.addTwoNumbers(l1,l2));
+    print_list(s.addList(l1, l2));
+    cout << "hello" << endl;
+    return 0;
+    
 }
