ce60b9fc514f334062797008f86b795c92a71ec9
henrytine <tianhangyu8886@gmail.com>
2020-04-14 09:01:02 +0800

    opt dp for maxSubArray

--- LeetCode/53.MaximumSubarray.cpp
+++ LeetCode/53.MaximumSubarray.cpp
@@ -24,26 +24,24 @@ public:
 };
 
 // dp
+
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,0);
+
         int len = nums.size();
-        dp[0] = nums[0];
-        for (int i = 1; i < len; ++i) {
-            if(dp[i-1] >= 0){
-                dp[i] = dp[i-1] + nums[i];
-            }
-            else{
-                dp[i] = nums[i];
-            }
-        }
-        // find max value in the dp
+        vector<int> dp(len + 1,0);
+        dp[0] = nums[0]; // init dp
         int res = dp[0];
-        for(int i = 1; i < len; i++){
-            res = max(dp[i],res);
+        for (int i = 1; i < len; ++i) {
+            
+            dp[i] = dp[i-1] > 0 ? (dp[i-1] + nums[i]) : nums[i];
+            res = max(res,dp[i]);
         }
+        
         return res;
     }
 };
+
 
