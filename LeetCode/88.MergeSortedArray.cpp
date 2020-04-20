92be1c0e0b978e19568bea17c5077f914c1030f6
henrytine <tianhangyu8886@gmail.com>
2020-04-20 13:14:27 +0800

    Update 88.MergeSortedArray.cpp

--- LeetCode/88.MergeSortedArray.cpp
+++ LeetCode/88.MergeSortedArray.cpp
@@ -41,17 +41,20 @@ public:
 
 
 // store the largest
+
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m -1, j = n - 1, k = m + n -1;
+        int i = m - 1, j = n - 1, k = m + n - 1;
         while(i >= 0 && j >= 0){
-            nums1[k--] = (nums1[i] < nums2[j]) ? nums2[j--] : nums1[i--];
+            nums1[k--] = (nums1[i] < nums2[j]) ? nums2[j--]:nums1[i--];
         }
         if(j >= 0){
-            while(j >= 0){
+            while(j>=0){
                 nums1[k--] = nums2[j--];
             }
         }
+        return;
     }
 };
