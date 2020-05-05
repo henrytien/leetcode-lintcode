a8b3493e2aba7b8b16f6a1c326ab3cc0aededd98
henrytine <tianhangyu8886@gmail.com>
2020-05-06 00:18:30 +0800

    Update 409.LongestPalindrome.py

--- LeetCode/Python3/409.LongestPalindrome.py
+++ LeetCode/Python3/409.LongestPalindrome.py
@@ -3,17 +3,18 @@
 # Time Complexity O(n)
 # Space Complexity O(128)
 # 先统计，然后成对计算
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         freqs = [0]*128
-        
+
         for c in s:
-            freqs[ord(c)]+=1 
-            
-        ans , odd = 0, 0
+            freqs[ord(c)] += 1
+
+        ans, odd = 0, 0
         for freq in freqs:
             ans += freq & (~1)
             odd |= freq & (1)
-            
+
         return ans + odd
-            
