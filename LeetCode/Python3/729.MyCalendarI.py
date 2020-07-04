65867009d5919159dcbc066195d9208122623d86
henrytine <tianhangyu8886@gmail.com>
2020-07-05 01:15:04 +0800

    Update 729.MyCalendarI.py

--- LeetCode/Python3/729.MyCalendarI.py
+++ LeetCode/Python3/729.MyCalendarI.py
@@ -36,3 +36,32 @@
 # 
 #####################################################################################################
 
+class MyCalendar:
+    
+    def __init__(self):
+        self.intervals = []
+
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        if end <= start:
+            return False
+        
+        i = bisect.bisect_right(self.intervals, start)
+        if i % 2:            # start is in some stored interval
+            return False
+        
+        j = bisect.bisect_left(self.intervals, end)
+        if i != j:
+            return False
+        
+        self.intervals[i:i] = [start, end]
+        return True
+        
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
