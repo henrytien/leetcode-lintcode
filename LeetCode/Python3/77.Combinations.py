f34fdb6686df5f22b631a37d040419c3a5af8ba3
henrytine <tianhangyu8886@gmail.com>
2020-07-04 09:31:10 +0800

    Create 77.Combinations.py

---
+++ LeetCode/Python3/77.Combinations.py
@@ -0,0 +1,23 @@
+# Source : https://leetcode.com/problems/combinations/
+# Author : henrytine
+# Date   : 2020-07-04
+
+##################################################################################################### 
+#
+# Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+# 
+# Example:
+# 
+# Input: n = 4, k = 2
+# Output:
+# [
+#   [2,4],
+#   [3,4],
+#   [2,3],
+#   [1,2],
+#   [1,3],
+#   [1,4],
+# ]
+# 
+#####################################################################################################
+
