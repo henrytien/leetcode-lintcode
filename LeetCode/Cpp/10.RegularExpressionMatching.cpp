be38cd38b55d54bc2ea04e99f27fa58f6ef8a367
henrytine <tianhangyu8886@gmail.com>
2020-07-16 08:03:49 +0800

    DP solution.

---
+++ LeetCode/Cpp/10.RegularExpressionMatching.cpp
@@ -0,0 +1,79 @@
+// Source : https://leetcode.com/problems/regular-expression-matching/
+// Author : henrytine
+// Date   : 2020-07-16
+
+/***************************************************************************************************** 
+ *
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for 
+ * '.' and '*'.
+ * 
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 	s could be empty and contains only lowercase letters a-z.
+ * 	p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * 
+ * Example 1:
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * Example 2:
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' 
+ * once, it becomes "aa".
+ * 
+ * Example 3:
+ * 
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * 
+ * Example 4:
+ * 
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+ * 
+ * Example 5:
+ * 
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ ******************************************************************************************************/
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m+1,vector<bool>(n+1,false));
+        
+        dp[0][0] = true;
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (p[j-1] != '*') {
+                    dp[i][j] = (i > 0 && dp[i-1][j-1]) && (s[i-1] == p[j-1] || p[j-1] == '.');
+                }else {
+                    dp[i][j] = dp[i][j-2] || ( i > 0 && dp[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
