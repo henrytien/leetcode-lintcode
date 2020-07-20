5cf75814d4da8404e6bb5007a6a8632dc6ad775a
henrytine <tianhangyu8886@gmail.com>
2020-07-21 00:07:32 +0800

    Add 155.

---
+++ leetcode/stack/155.min_stack/155.MinStack_henrytine.py
@@ -0,0 +1,70 @@
+# Source : https://leetcode.com/problems/min-stack/
+# Author : henrytine
+# Date   : 2020-07-21
+
+##################################################################################################### 
+#
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+# 
+# 	push(x) -- Push element x onto stack.
+# 	pop() -- Removes the element on top of the stack.
+# 	top() -- Get the top element.
+# 	getMin() -- Retrieve the minimum element in the stack.
+# 
+# Example 1:
+# 
+# Input
+# ["MinStack","push","push","push","getMin","pop","top","getMin"]
+# [[],[-2],[0],[-3],[],[],[],[]]
+# 
+# Output
+# [null,null,null,null,-3,null,0,-2]
+# 
+# Explanation
+# MinStack minStack = new MinStack();
+# minStack.push(-2);
+# minStack.push(0);
+# minStack.push(-3);
+# minStack.getMin(); // return -3
+# minStack.pop();
+# minStack.top();    // return 0
+# minStack.getMin(); // return -2
+# 
+# Constraints:
+# 
+# 	Methods pop, top and getMin operations will always be called on non-empty stacks.
+#####################################################################################################
+
+class MinStack:
+    
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack or x <= self.min_stack[-1]:
+            self.min_stack.append(x)
+
+    def pop(self) -> None:
+        if self.min_stack[-1] == self.stack[-1]:
+             self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
