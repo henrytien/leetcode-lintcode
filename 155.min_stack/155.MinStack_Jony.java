1804e690e9daf7941bdd8df21801e21b5ef6c9e6
Jony <834810096@qq.com>
2020-07-26 20:26:56 +0800

    two stack

--- 155.min_stack/155.MinStack_Jony.java
+++ 155.min_stack/155.MinStack_Jony.java
@@ -36,3 +36,37 @@
  ******************************************************************************************************/
 
 
+import java.util.Stack;
+class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    /** initialize your data structure here. */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        if(stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
