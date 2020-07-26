ef392e108025400580463ca6fd0c00585b3f291c
henrytine <tianhangyu8886@gmail.com>
2020-07-26 23:35:29 +0800

    Update 33.SearchinRotatedSortedArray_henrytine.cpp

--- leetcode/binary_search/33.search_in_rotated_sorted_array/33.SearchinRotatedSortedArray_henrytine.cpp
+++ leetcode/binary_search/33.search_in_rotated_sorted_array/33.SearchinRotatedSortedArray_henrytine.cpp
@@ -28,28 +28,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int start = 0, end = n - 1;
+        int n = nums.size(), start = 0, end = n - 1, mid = 0;
+        
         while (start <= end) {
-            int mid = (start - end) / 2 + end;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            else if (nums[mid] >=  nums[start]) {
-                if (target >= nums[start] && target < nums[mid]) 
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] >= nums[start]) {
+                if (target < nums[mid] && target >= nums[start]) {
                     end = mid - 1;
+                }
                 else {
                     start = mid + 1;
                 }
             }
             else {
-                if (target <= nums[end] && target > nums[mid]) 
+                if (target > nums[mid] && target <= nums[end]) {
                     start = mid + 1;
-                else{
+                }
+                else {
                     end = mid - 1;
                 }
-            }            
+            }
         }
-        return -1;
+        return - 1;
     }
 };
