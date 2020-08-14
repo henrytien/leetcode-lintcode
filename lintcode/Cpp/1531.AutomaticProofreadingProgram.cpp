703df6683a8aac0872f92cf2a69c040ca4774488
henrytine <tianhangyu8886@gmail.com>
2020-08-14 08:07:01 +0800

    Create 1531.AutomaticProofreadingProgram.cpp

---
+++ lintcode/Cpp/1531.AutomaticProofreadingProgram.cpp
@@ -0,0 +1,36 @@
+// https://www.lintcode.com/problem/automatic-proofreading-program/description?_from=ladder&&fromId=157
+class Solution {
+public:
+    /**
+     * @param str: The string before proofreading.
+     * @return: Return the string after proofreading.
+     */
+    string automaticProofreadingProgram(string &str) {
+        vector<char> result;
+        for (char c: str) {
+            result.push_back(c);
+            int n = result.size();
+            // 规则1
+            if (n >= 3) {
+                if (result[n - 1] == result[n - 2]
+                    && result[n - 1] == result[n - 3]) {
+                    result.pop_back();
+                }
+            }
+            n = result.size();
+            // 规则2
+            if (n >= 4) {
+                if (result[n - 1] == result[n - 2]
+                    && result[n - 3] == result[n - 4]) {
+                    
+                    result.pop_back();
+                }
+            }
+        }
+        string resultStr = "";
+        for (char c: result) {
+            resultStr += c;
+        }
+        return resultStr;
+    }
+};
