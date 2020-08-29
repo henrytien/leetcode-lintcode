aece7788e7f981ab15f5d3cd455688eb7ae68cad
henrytine <tianhangyu8886@gmail.com>
2020-08-29 20:59:27 +0800

    Update 94.BinaryTreeInorderTraversal_henrytine.cpp

--- leetcode/binary_tree/94.binary_tree_inorder_traversal/94.BinaryTreeInorderTraversal_henrytine.cpp
+++ leetcode/binary_tree/94.binary_tree_inorder_traversal/94.BinaryTreeInorderTraversal_henrytine.cpp
@@ -49,3 +49,38 @@ private:
             helper(node->right,res);
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        
+        while (root || !s.empty()) {
+            while (root) {
+                s.push(root);
+                root = root->left; 
+            }
+            if (!s.empty()) {
+                root = s.top();
+                s.pop();
+                res.push_back(root->val);
+                root = root->right;
+            }
+            
+        }
+        return res;
+    }
+};
