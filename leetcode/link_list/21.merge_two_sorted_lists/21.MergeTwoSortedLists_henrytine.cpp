53525de95ac4f0a5878445e3d4c64ff350251bb3
henrytine <tianhangyu8886@gmail.com>
2020-10-11 11:02:15 +0800

    21

---
+++ leetcode/link_list/21.merge_two_sorted_lists/21.MergeTwoSortedLists_henrytine.cpp
@@ -0,0 +1,63 @@
+// Source : https://leetcode.com/problems/merge-two-sorted-lists/
+// Author : henrytine
+// Date   : 2020-10-11
+
+/***************************************************************************************************** 
+ *
+ * Merge two sorted linked lists and return it as a new sorted list. The new list should be made by 
+ * splicing together the nodes of the first two lists.
+ * 
+ * Example 1:
+ * 
+ * Input: l1 = [1,2,4], l2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * Example 2:
+ * 
+ * Input: l1 = [], l2 = []
+ * Output: []
+ * 
+ * Example 3:
+ * 
+ * Input: l1 = [], l2 = [0]
+ * Output: [0]
+ * 
+ * Constraints:
+ * 
+ * 	The number of nodes in both lists is in the range [0, 50].
+ * 	-100 <= Node.val <= 100
+ * 	Both l1 and l2 are sorted in non-decreasing order.
+ ******************************************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        
+        if (l1) tail->next = l1;
+        if (l2) tail->next = l2;
+        return dummy.next;
+    }
+};
