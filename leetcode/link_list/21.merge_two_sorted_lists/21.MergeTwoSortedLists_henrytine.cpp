20a697908f4310327704d5d3de98c511366633a4
henrytine <tianhangyu8886@gmail.com>
2020-10-11 13:59:49 +0800

    recursion

--- leetcode/link_list/21.merge_two_sorted_lists/21.MergeTwoSortedLists_henrytine.cpp
+++ leetcode/link_list/21.merge_two_sorted_lists/21.MergeTwoSortedLists_henrytine.cpp
@@ -61,3 +61,29 @@ public:
         return dummy.next;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1 || !l2) return l1 ? l1:l2;
+        
+        if (l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    }
+};
