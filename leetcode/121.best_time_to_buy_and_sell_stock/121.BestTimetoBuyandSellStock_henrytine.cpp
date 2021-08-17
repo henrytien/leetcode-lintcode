2a7f56c0f8a35366a999d3045156588ff10494a7
henrytien <tianhangyu8886@gmail.com>
2021-08-17 14:21:26 +0000

    ac

--- leetcode/121.best_time_to_buy_and_sell_stock/121.BestTimetoBuyandSellStock_henrytine.cpp
+++ leetcode/121.best_time_to_buy_and_sell_stock/121.BestTimetoBuyandSellStock_henrytine.cpp
@@ -43,3 +43,22 @@ public:
         return maxProfit;
     }
 };
+
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() == 0) {
+            return 0;
+        }
+
+        int buy = prices[0];
+        int profit = 0;
+        for (int i  = 1; i < prices.size(); i++) {
+            if (buy > prices[i]) {
+                buy = prices[i];
+            }
+            profit = max(profit,prices[i] - buy);
+        }
+        return profit;
+    }
+};
